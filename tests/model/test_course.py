356cc080add407886a629b20fe164a110ed94088
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-07-30 10:08:21 -0700

    test: pre-populate test database
    Populate the test database with test data during setup.
    This way, tests can be more independent and there's less redundancy.

--- tests/model/test_course.py
+++ tests/model/test_course.py
@@ -2,49 +2,49 @@
 import pytest
 from sqlalchemy.orm import Session
 
-from bcitflex.model import Course, Offering
+from bcitflex.model import Course
 
 
 @pytest.fixture
-def course(offering) -> Course:
-    """Return a test course."""
+def new_course(new_offering) -> Course:
+    """Return a new course object."""
     return Course(
-        course_id=1,
+        course_id=2,
         subject_id="COMP",
         code="1234",
         name="Test Course",
         prerequisites="COMP 1000",
         credits=3.0,
         url="https://www.bcit.ca",
-        offerings=[offering],
+        offerings=[new_offering],
     )
 
 
 class TestCourse:
     """Test the Course class."""
 
-    def test_init(self, course: Course, offering: Offering) -> None:
+    def test_init(self, new_course: Course, new_offering) -> None:
         """Test the constructor."""
-        assert course.course_id == 1
-        assert course.subject_id == "COMP"
-        assert course.code == "1234"
-        assert course.name == "Test Course"
-        assert course.prerequisites == "COMP 1000"
-        assert course.credits == 3.0
-        assert course.url == "https://www.bcit.ca"
-        assert course.offerings == [offering]
+        assert new_course.course_id == 2
+        assert new_course.subject_id == "COMP"
+        assert new_course.code == "1234"
+        assert new_course.name == "Test Course"
+        assert new_course.prerequisites == "COMP 1000"
+        assert new_course.credits == 3.0
+        assert new_course.url == "https://www.bcit.ca"
+        assert new_course.offerings == [new_offering]
 
 
 class TestCourseDB:
     """Test the Course class with a database session."""
 
-    def test_add_course(self, course: Course, session: Session) -> None:
+    def test_add_course(self, new_course: Course, session: Session) -> None:
         """Test adding a course to the db."""
-        session.add(course)
+        session.add(new_course)
         session.commit()
-        assert session.get(Course, 1) == course
+        assert session.get(Course, 2) == new_course
 
-    def test_update_course(self, course: Course, session: Session) -> None:
+    def test_update_course(self, session: Session) -> None:
         """Test updating a course in the db."""
         course = session.get(Course, 1)
         course.name = "New Name"
