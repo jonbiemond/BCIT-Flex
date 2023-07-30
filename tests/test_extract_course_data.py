3a3d5603af17d9b849e0a7904ef3704b740190da
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-07-30 10:24:17 -0700

    feat: factor out course parsing logic
    Abstract course parsing logic into separate function.
    Add CoursePage class to couple url to HTMLParser.

--- tests/test_extract_course_data.py
+++ tests/test_extract_course_data.py
@@ -2,30 +2,34 @@
 from pickle import load
 
 import pytest
-from requests import Response
-from selectolax.parser import HTMLParser, Node
+from selectolax.parser import Node
 
-from bcitflex.modules.extract_course_data import next_term, parse_offering_node
+from bcitflex.modules.extract_course_data import (
+    CoursePage,
+    parse_course_info,
+    parse_offering_node,
+)
 
 
 @pytest.fixture
-def course_response() -> Response:
+def course_page() -> CoursePage:
     """Return a test course response."""
-    return load(open("tests/test_data/course_response.pkl", "rb"))
+    return CoursePage(load(open("tests/test_data/course_response.pkl", "rb")))
 
 
 @pytest.fixture
-def offering_node(course_response) -> Node:
+def offering_node(course_page) -> Node:
     """Return a test offering node."""
-
-    term = next_term(course_response)
-
-    html = HTMLParser(course_response.text)
-    return html.css_first(f'div[id="{term}"] div[class="sctn"]')
+    return course_page.html.css_first(f'div[id="{course_page.term}"] div[class="sctn"]')
 
 
 def test_parse_offering_node(offering_node: Node) -> None:
     """Test the parse offering node function."""
-
     offering = parse_offering_node(offering_node)
     assert offering.price > 0
+
+
+def test_parse_course_info(course_page) -> None:
+    """Test the parse course function."""
+    course = parse_course_info(course_page)
+    assert course.credits > 0
