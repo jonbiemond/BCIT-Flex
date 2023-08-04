9970baa8dd8908381215ef4d59e5c5473e477db9
Jonathan Biemond <jonbiemond@gmail.com>
2023-08-03 19:47:15 -0700

    feat: add prep_db and scrape_course_urls
    - Add course_list_response.pkl to test data.

--- tests/load_test_data.py
+++ tests/load_test_data.py
@@ -5,9 +5,13 @@ from bcitflex.modules.extract_course_data import collect_response
 
 if __name__ == "__main__":
     base_url = "https://www.bcit.ca"
-    course_url = r"/courses/business-analysis-and-systems-design-comp-2831/"
-    url = base_url + course_url
+    course_url = "/courses/business-analysis-and-systems-design-comp-2831/"
+    course_list_url = "/wp-json/bcit/ptscc/v1/list-active-urls"
 
-    # get response
-    response = collect_response(url)
+    # get course response
+    response = collect_response(base_url + course_url)
     dump(response, open("./test_data/course_response.pkl", "wb"))
+
+    # get course list response
+    response = collect_response(base_url + course_list_url)
+    dump(response, open("./test_data/course_list_response.pkl", "wb"))
