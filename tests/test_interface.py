cae9ce10de7f9d19503f922b330206abbd64719f
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-08-05 19:54:15 -0700

    tests: add @dbtest mark to db dependant texts
    Add @dbtest decorator for tests that connect to the database.
    Use non-db fixture in test_parse_offering_node.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,8 +2,10 @@
 from sqlalchemy.orm import Session
 
 from bcitflex.interface import valid_subject
+from tests import dbtest
 
 
+@dbtest
 def test_valid_subject(session: Session) -> None:
     """Test valid_subject returns subject validity."""
     assert valid_subject(session, "COMP") is True
