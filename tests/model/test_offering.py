020b848df272f080afe7e080edfb114d1aaefaf2
Jonathan Biemond <jonbiemond@gmail.com>
2023-08-10 12:35:15 -0700

    feat(model): expose Meeting attributes in Offering

--- tests/model/test_offering.py
+++ tests/model/test_offering.py
@@ -1,4 +1,6 @@
 """Tests for the offering model."""
+import datetime
+
 import pytest
 from sqlalchemy.exc import DataError
 from sqlalchemy.orm import Session
@@ -78,3 +80,40 @@ class TestOfferingDB:
             except DataError as e:
                 session.rollback()
                 raise e
+
+
+@dbtest
+class TestPrimaryMeeting:
+    """Test the primary meeting and attributes of the offering class."""
+
+    def test_get_primary_meeting(self, session: Session) -> None:
+        """Test offering from database includes primary meeting."""
+        offering = session.get(Offering, "12345")
+        assert offering.primary_meeting.meeting_id == 1
+
+    def test_simple_properties(self, session: Session) -> None:
+        """Test access attributes of primary meeting through offering."""
+        offering = session.get(Offering, "12345")
+        assert offering.start_time == datetime.time(18)
+        assert offering.end_time == datetime.time(21)
+        assert offering.campus == "Online"
+
+    def test_aggregate_properties(self, session: Session) -> None:
+        """Test properties that query against all related meetings."""
+
+        # add new meeting to db
+        new_meeting = Meeting(
+            meeting_id=2,
+            crn="12345",
+            days=["Thu", "Fri"],
+            start_date=datetime.date(2023, 8, 1),
+            end_date=datetime.date(2023, 12, 1),
+        )
+        session.add(new_meeting)
+        session.commit()
+
+        # get offering
+        offering = session.get(Offering, "12345")
+        assert offering.start_date == datetime.date(2023, 8, 1)
+        assert offering.end_date == datetime.date(2023, 12, 1)
+        assert offering.days == ["Wed", "Thu", "Fri"]
