ccd9b6af5b3f4c297c5e6e9498cef67b922636ba
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-09-02 17:50:03 -0700

    feat(app): add auth Blueprint

--- bcitflex/__init__.py
+++ bcitflex/__init__.py
@@ -35,4 +35,8 @@ def create_app(test_config=None):
 
     db.init_app(app)
 
+    from . import auth
+
+    app.register_blueprint(auth.bp)
+
     return app
---
+++ bcitflex/auth.py
@@ -0,0 +1,100 @@
+"""Authentication Blueprint"""
+import functools
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from bcitflex.db import DBSession
+from bcitflex.model import User
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = DBSession()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                new_user = User(
+                    username=username, password=generate_password_hash(password)
+                )
+                with db.begin():
+                    db.add(new_user)
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = DBSession()
+        error = None
+        user = User.get_by_unique(db, username)
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = DBSession().get(User, user_id)
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
