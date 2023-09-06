f7e2902753970927d34ae10e6a63f2d8d87286d1
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-09-06 04:15:43 -0700

    fix(app): add metadata to app db context

--- bcitflex/ext/database.py
+++ bcitflex/ext/database.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy as SQLAlchemyBase
+from sqlalchemy.orm import DeclarativeBase
 
 
 class SQLAlchemy(SQLAlchemyBase):
-    """Flask extension that integrates alchy with Flask-SQLAlchemy."""
+    """Flask extension that integrates DeclarativeBase with Flask-SQLAlchemy."""
 
-    def __init__(self, app=None, session_options=None, model=None):
+    def __init__(self, model: DeclarativeBase, app=None, session_options=None):
         self.Model = model
-        super(SQLAlchemy, self).__init__(app=app, session_options=session_options)
+        super(SQLAlchemy, self).__init__(
+            app=app, session_options=session_options, metadata=model.metadata
+        )
 
     def _make_declarative_base(self, _):
         """Creates or extends the declarative base."""
