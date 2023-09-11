889b44dbbb3b8eb4486da59d5ff72231e5c3fd58
Jonathan Biemond <jonathan.biemond@devrygreenhouses.com>
2023-09-11 10:09:22 -0700

    chore: drop unused db_config.py

--- bcitflex/model/db_config.py
+++
@@ -1,8 +0,0 @@
-"""Database connection objects"""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-# Create engine and session
-engine = create_engine("postgresql://python_app@localhost:5432/bcitflex")
-session = Session(engine)
