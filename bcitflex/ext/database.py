40c53170903d2b51f9479696a8f88a4ce618d14d
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-09-17 08:54:35 -0700

    feat(model): add support for ORM joins to soft delete hook

--- bcitflex/ext/database.py
+++ bcitflex/ext/database.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy as SQLAlchemyBase
-from sqlalchemy import Table
+from sqlalchemy import FromClause, Join, Table
 from sqlalchemy.event import listens_for
 from sqlalchemy.orm import DeclarativeBase, ORMExecuteState, Session
+from sqlalchemy.orm.util import _ORMJoin
 from sqlalchemy.sql import (
     Executable,
     Select,
@@ -28,18 +29,36 @@ def rewrite_statement(stmt: Executable) -> Select:
 
     if isinstance(stmt, Select):
         for from_clause in stmt.get_final_froms():
-            if isinstance(from_clause, Table):
-                deleted_field = from_clause.columns.get("deleted_at")
-                stmt = stmt.where(deleted_field.is_(None))
-
-            else:
-                raise NotImplementedError(
-                    f'Unsupported froms type "{(type(from_clause))}"!'
-                )
+            stmt = rewrite_from_clause(stmt, from_clause)
 
         return stmt
 
     raise NotImplementedError(f'Unsupported statement type "{(type(stmt))}"!')
+
+
+def rewrite_from_clause(stmt: Select, from_clause: FromClause):
+    """Rewrite a single from clause."""
+
+    if isinstance(from_clause, Table):
+        deleted_field = from_clause.columns.get("deleted_at")
+        stmt = stmt.where(deleted_field.is_(None))
+
+    elif isinstance(from_clause, _ORMJoin):
+        # not expecting recursive joins
+        if any(
+            type(j) in (_ORMJoin, Join) for j in (from_clause.left, from_clause.right)
+        ):
+            raise NotImplementedError(
+                f"Recursive joins are not supported! Join: {from_clause}"
+            )
+        # rewrite the left and right sides of the join
+        stmt = rewrite_from_clause(stmt, from_clause.left)
+        stmt = rewrite_from_clause(stmt, from_clause.right)
+
+    else:
+        raise NotImplementedError(f'Unsupported froms type "{(type(from_clause))}"!')
+
+    return stmt
 
 
 @listens_for(Session, identifier="do_orm_execute")
--- tests/ext/test_database.py
+++ tests/ext/test_database.py
@@ -1,8 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import and_, select
 from sqlalchemy.orm import Session
 
 from bcitflex.ext.database import rewrite_statement
-from bcitflex.model import User
+from bcitflex.model import Course, Offering, User
 from tests import dbtest
 
 
@@ -20,13 +20,26 @@ class TestSoftDelete:
         # assert we are excluding soft-deleted objects
         assert stmt.whereclause.compare(User.deleted_at.is_(None))
 
+    def test_orm_join(self):
+        """Test that an ORM join statement is rewritten."""
+        stmt = select(Course).join(Offering)
+        stmt = rewrite_statement(stmt)
+
+        # if we don't have a where clause, we can't be filtering for soft-deleted
+        assert stmt.whereclause is not None
+
+        # assert we are excluding soft-deleted objects
+        assert stmt.whereclause.compare(
+            and_(Offering.deleted_at.is_(None), Course.deleted_at.is_(None))
+        )
+
 
 @dbtest
 class TestSoftDeleteDB:
     """Test the SoftDelete extension with the database."""
 
-    def test_hook(self, session: Session):
-        """Test event listener intercepts statement."""
+    def test_select(self, session: Session):
+        """Test that the event hook intercepts a select statement."""
 
         user_ct = len(session.scalars(select(User)).all())
 
@@ -38,6 +51,19 @@ class TestSoftDeleteDB:
 
         # assert we are excluding soft-deleted objects
         assert len(session.scalars(select(User)).all()) == user_ct
+
+    def test_load_children(self, session: Session, new_offering: Offering):
+        """Test that the select hook also applies to loading children."""
+
+        # add deleted offering to database
+        deleted_offering = new_offering
+        deleted_offering.deleted_at = "2021-01-01"
+        course_id = deleted_offering.course_id
+        session.add(deleted_offering)
+        session.commit()
+
+        # assert soft-deleted objects are not loaded
+        assert deleted_offering not in session.get(Course, course_id).offerings
 
     def test_include_option(self, session: Session):
         """Test that the include_deleted option includes deleted objects."""
