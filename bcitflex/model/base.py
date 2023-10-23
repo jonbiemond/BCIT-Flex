29c6cb0337355643e7fb35639b3c547f78084055
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-10-23 05:25:46 -0700

    fix(db): include all columns in unique constraint name

--- bcitflex/model/base.py
+++ bcitflex/model/base.py
@@ -19,8 +19,8 @@ _T = TypeVar("_T", bound="Base")
 
 # Constraint naming conventions
 convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ix": "ix_%(column_0_N_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_N_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s",
