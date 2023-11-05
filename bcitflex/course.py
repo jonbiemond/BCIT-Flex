d8794b1264a29dfc53767f70671e93cfdb9a5067
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-11-05 10:28:42 -0800

    feat(app): implement course filters as SQL
    Apply course filters via SQL instead of in state.
    It is more efficient to filter courses at the database level.

--- bcitflex/course.py
+++ bcitflex/course.py
@@ -1,36 +1,30 @@
 """Course Blueprint"""
 
 from flask import Blueprint, render_template, request
-from sqlalchemy import select
-from werkzeug.datastructures import ImmutableMultiDict
+from sqlalchemy import not_, select
 
 from bcitflex.app_functions import ModelFilter
-from bcitflex.app_functions.course_query import Match
 from bcitflex.db import DBSession
 from bcitflex.model import Course, Meeting, Offering, Subject, Term
+from bcitflex.model.offering import NOT_AVAILABLE
 
 bp = Blueprint("course", __name__)
-
-
-def filters_from_form(form: ImmutableMultiDict) -> ModelFilter:
-    """Return a list of CourseFilters from the given form."""
-    filters = ModelFilter(Course)
-    filters.add_condition("term_id", form.get("term"), Offering)
-    filters.add_condition("subject_id", form.get("subject"))
-    filters.add_condition("campus", form.get("campus"), Offering)
-    filters.add_condition("code", form.get("code"))
-    filters.add_condition("name", form.get("name"), None, Match.PARTIAL)
-    filters.add_condition("is_available", form.get("available") == "True" or None)
-    return filters
 
 
 @bp.route("/")
 @bp.route("/courses", methods=["GET", "POST"])
 def index():
-    courses = DBSession.scalars(select(Course)).all()
+    filters = ModelFilter(Course)
     if request.method == "POST":
-        print(request.form)
-        courses = filters_from_form(request.form).filter(courses)
+        filters.where(Offering.term_id == request.form.get("term"))
+        filters.where(Course.subject_id == request.form.get("subject"))
+        filters.where(Meeting.campus == request.form.get("campus"))
+        filters.where(Course.code == request.form.get("code"))
+        if name := request.form.get("name"):
+            filters.where(Course.name.ilike("%" + name + "%"))
+        if request.form.get("available") is not None:
+            filters.where(not_(Offering.status.in_(NOT_AVAILABLE)))
+    courses = DBSession.scalars(filters.stmt).all()
     subjects = DBSession.scalars(select(Subject).where(Subject.is_active)).all()
     terms = DBSession.scalars(select(Term).join(Offering)).unique().all()
     locations = DBSession.scalars(select(Meeting.campus).distinct()).all()
